d41f9785e308f839746bcc6e552b9f11367c462a
ASHIRBAD-PRADHAN <pradhanashirbad18@gmail.com>
2025-08-02 13:10:51 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,29 @@
+class Arithmatic
+{
+public static void main(String[] args)
+{
+
+     int v1=55,v2=5;
+System.out.println("Addition of two value (v1+v2):" +(v1+v2));
+System.out.println("subtraction of two value (v1-v2):" +(v1-v2));
+System.out.println("multipication of two value (v1*v2):" +(v1*v2));
+System.out.println("division of two value (v1/v2):" +(v1/v2));
+System.out.println("mod division of two value (v1%v2):" +(v1%v2));
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
